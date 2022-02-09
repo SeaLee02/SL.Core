c29f3c45bc90623ecca0381e1e16cb8e99b0136a
sealee02 <lhkl1314>
2022-02-09 18:16:32 +0800

    修改项目引用

---
+++ SL/02_src/06_Mapper/Mapper/IMapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SL.Mapper
+{
+    public interface IMapperConfig
+    {
+        /// <summary>
+        /// 绑定
+        /// </summary>
+        /// <param name="cfg"></param>
+        void Bind(IMapperConfigurationExpression cfg);
+    }
+}
